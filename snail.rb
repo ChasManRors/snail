d7ebf58a7675e9392614b10c73d3fbecefdea619
Charles Magid <cmmagid@gmail.com>
2021-02-28 22:53:27 -0500

    minor refactor

--- snail.rb
+++ snail.rb
@@ -2,41 +2,37 @@
 require 'fileutils'
 require 'pry'
 
-class Snail
-  def initialize; end
+def snail(number)
+  x_max = y_max = number.first.size - 1
+  x_min = y_min = 0
+  array_size = (x_max + 1)**2
+  answer = []
 
-  def snail(number)
-    x_max = y_max = number.first.size - 1
-    x_min = y_min = 0
-    array_size = (x_max + 1)**2
-    answer = []
+  while true
 
-    while true
+    xs = (x_min..x_max).to_a
+    y = y_min
+    answer += xs.map { |x| number[y][x] }
+    y_min += 1 # => push down the top
+    return answer if answer.size == array_size
 
-      xs = (x_min..x_max).to_a
-      y = y_min
-      answer << xs.map { |x| number[y][x] }
-      y_min +=  1 # => push down the top
-      return answer.flatten if answer.flatten.size == array_size
+    x = x_max
+    ys = (y_min..y_max).to_a
+    answer += ys.map { |y| number[y][x] }
+    x_max -= 1 # => move the right side inward
+    return answer if answer.size == array_size
 
-      x = x_max
-      ys = (y_min..y_max).to_a
-      answer << ys.map { |y| number[y][x] }
-      x_max -= 1 # => move the right side inward
-      return answer.flatten if answer.flatten.size == array_size
+    xs = (x_min..x_max).to_a.reverse
+    y = y_max
+    answer += xs.map { |x| number[y][x] }
+    y_max -= 1 # => move up the bottom
+    return answer if answer.size == array_size
 
-      xs = (x_min..x_max).to_a.reverse
-      y = y_max
-      answer << xs.map { |x| number[y][x] }
-      y_max -= 1 # => move up the bottom
-      return answer.flatten if answer.flatten.size == array_size
+    x = x_min
+    ys = (y_min..y_max).to_a.reverse
+    answer += ys.map { |y| number[y][x] }
+    x_min += 1 # => move the left side in
+    return answer if answer.size == array_size
 
-      x = x_min
-      ys = (y_min..y_max).to_a.reverse
-      answer << ys.map { |y| number[y][x] }
-      x_min += 1 # => move the left side in
-      return answer.flatten if answer.flatten.size == array_size
-
-    end
   end
 end
--- spec/snail_spec.rb
+++ spec/snail_spec.rb
@@ -2,14 +2,13 @@
 require 'spec_helper'
 require_relative '../snail'
 
-describe Snail do
-  let(:f) { Snail.new }
+describe "Snail" do
   let(:input) { [[1, 2, 3], [4, 5, 6], [7, 8, 9]] }
   let(:expected) { [1, 2, 3, 6, 9, 8, 7, 4, 5] }
 
   context 'given an nxn array' do
-    it 'returns snail path sequence' do
-      expect(f.snail(input)).to eq(expected)
+    it 'returns snail path sequence through array' do
+      expect(snail(input)).to eq(expected)
     end
   end
 end
