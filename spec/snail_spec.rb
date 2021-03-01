15a3b6c456f6052882fce17da53f849352d4cba9
Charles Magid <cmmagid@gmail.com>
2021-02-28 22:29:50 -0500

    initial commit of snail path through square array

---
+++ spec/snail_spec.rb
@@ -0,0 +1,15 @@
+# create your sample spec in the root/spec dir
+require 'spec_helper'
+require_relative '../snail'
+
+describe Snail do
+  let(:f) { Snail.new }
+  let(:input) { [[1, 2, 3], [4, 5, 6], [7, 8, 9]] }
+  let(:expected) { [1, 2, 3, 6, 9, 8, 7, 4, 5] }
+
+  context 'given an nxn array' do
+    it 'returns snail path sequence' do
+      expect(f.snail(input)).to eq(expected)
+    end
+  end
+end
