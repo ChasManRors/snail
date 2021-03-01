5c88d2b1811cde97352e9ef840e19a74c39bbb18
Charles Magid <cmmagid@gmail.com>
2021-03-01 12:49:13 -0500

    need to get this to work

--- snail.rb
+++ snail.rb
@@ -1,12 +1,14 @@
 # Alternatively use #!/usr/bin/env ruby & cmdline.rb
 require 'fileutils'
 require 'pry'
+require 'json'
+require 'set'
 
-def snail(array)
+def snail_accepted(array)
   array.empty? ? [] : array.shift + snail(array.transpose.reverse)
 end
 
-def snail_orig(number)
+def snail_charles(number)
   x_max = y_max = number.first.size - 1
   x_min = y_min = 0
   array_size = (x_max + 1)**2
@@ -37,4 +39,66 @@ def snail_orig(number)
     x_min += 1 # => move the left side in
     return answer if answer.size == array_size
   end
+end
+
+def run(vector)
+  current_loc = { row: 0, column: 0 }
+  direction = :east
+  memory = Set[vector[0][0]]
+  move(direction, current_loc, vector, memory, memory.to_a)
+end
+
+def next_direction(current_direction)
+  directions = %i[east south west north]
+  current_idx = directions.find_index(current_direction)
+  current_idx + 1 < directions.size ? (directions[current_idx + 1]) : directions[0]
+end
+
+def next_position(direction, current_loc)
+  # puts "currently at: #{current_loc.inspect} & facing #{direction}"
+  case direction
+  when :east
+    current_loc.merge({ column: current_loc[:column] + 1 })
+  when :south
+    current_loc.merge({ row: current_loc[:row] + 1 })
+  when :west
+    current_loc.merge({ column: current_loc[:column] - 1 })
+  when :north
+    current_loc.merge({ row: current_loc[:row] - 1 })
+  end
+end
+
+def valid_position?(possible_next_position, vector)
+  vector.size > possible_next_position[:row]\
+    && vector[possible_next_position[:row]].size > possible_next_position[:column]
+end
+
+def move(direction, current_loc, vector, memory, answer)
+  return answer if answer.size == (vector.size * vector[0].size)
+
+  # puts "move: current_answer: #{answer.inspect}"
+  possible_next_position = next_position(direction, current_loc)
+  # puts "possible_next_position: #{possible_next_position.inspect} & facing #{direction}"
+  if valid_position?(possible_next_position, vector)
+    next_number = vector[possible_next_position[:row]][possible_next_position[:column]]
+    if !memory.include? next_number
+      answer << next_number
+      memory << answer.last
+      move(direction, possible_next_position, vector, memory, answer)
+    else
+      turn_and_move(direction, current_loc, vector, memory, answer)
+    end
+  else
+    turn_and_move(direction, current_loc, vector, memory, answer)
+  end
+end
+
+def turn_and_move(direction, current_loc, vector, memory, answer)
+  thataway = next_direction(direction)
+  # puts "next direction: #{thataway}"
+  move(thataway, current_loc, vector, memory, answer)
+end
+
+def snail(array)
+  run(array.join(' '))
 end
